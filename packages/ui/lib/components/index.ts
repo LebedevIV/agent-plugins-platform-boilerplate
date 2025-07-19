ac7a6b0a032eb5e66ab6aa00c4754f0906a19c09
Igor Lebedev <igor_samodurov@mail.ru>
2025-07-20 02:18:48 +0500

    fix: ui: export ToggleButton component to resolve ReferenceError in side-panel

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -1,5 +1,5 @@
 // export * from './TestReact19Pure';
-// export * from './ToggleButton';
+export * from './ToggleButton';
 // export * from './LoadingSpinner';
 // export * from './error-display/ErrorDisplay';
 // export * from './error-display/ErrorResetButton';
