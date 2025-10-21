2cebc89174024bc2eea6b5fc9ace74718f9ed8e0
Igor Lebedev <lebedev.igor.viktorovich@gmail.com>
2025-10-22 04:55:26 +0500

    восстановлена функциональнсть на новой ОС

--- packages/zipper/lib/zip-bundle.ts
+++ packages/zipper/lib/zip-bundle.ts
@@ -48,7 +48,7 @@ export const zipBundle = async (
     let aborted = false;
     let totalSize = 0;
     const timer = Date.now();
-    const zip = new Zip((err, data, final) => {
+    const zip = new Zip((err: Error | null, data: Uint8Array, final: boolean) => {
       if (err) {
         pReject(err);
       } else {
